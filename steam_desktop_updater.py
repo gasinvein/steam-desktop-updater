127371aef4d8fed9f3788367b5647b8ee9afe174
gasinvein <gasinvein@gmail.com>
2018-10-01 22:55:57 +0300

    Rewrited without involving a temp directory

--- steam_desktop_updater.py
+++ steam_desktop_updater.py
@@ -6,10 +6,9 @@ import sys
 import os
 import glob
 import zipfile
-import shutil
 from configparser import ConfigParser
 from PIL import Image
-from tempfile import gettempdir
+import io
 
 
 def get_installed_apps(library_folder):
@@ -23,62 +22,9 @@ def get_installed_apps(library_folder):
             yield app_mainfest['AppState']['appid']
 
 
-def extract_zipped_icons(tmpdir, icon_hash, icon_path):
-    icons = {}
-    with zipfile.ZipFile(icon_path, 'r') as zf:
-        for zi in zf.infolist():
-            if not zi.is_dir() and zi.filename.endswith('.png'):
-                with zf.open(zi.filename) as img_file:
-                    try:
-                        img = Image.open(img_file)
-                    except OSError as e:
-                        print(zi.filename, ":", e, file=sys.stderr)
-                    else:
-                        h, w = img.size
-                        if h == w:
-                            dest = os.path.join(tmpdir, icon_hash)
-                            print('Extracting', zi.filename, file=sys.stderr)
-                            zf.extract(zi.filename, dest)
-                            icons[h] = os.path.join(dest, zi.filename)
-                    finally:
-                        img.close()
-    return icons
-
-
-def extract_icons(steam_root, icon_hash, icon_path):
+def get_icon_source(steam_root, app_info):
     """
-    Extract zipped icons and convert ones in non-png format to png
-    """
-    icons = {}
-    tmpdir = os.path.join(gettempdir(), 'steam-icons')
-    os.makedirs(tmpdir, exist_ok=True)
-    if icon_path.endswith('.zip'):
-        zipped_icons = extract_zipped_icons(tmpdir=tmpdir, icon_hash=icon_hash, icon_path=icon_path)
-        icons.update(zipped_icons)
-    elif icon_path.endswith('.ico'):
-        try:
-            with Image.open(icon_path) as img:
-                h, w = img.size
-                if h == w:
-                    dest = os.path.join(tmpdir, icon_hash + '.png')
-                    img.save(dest, 'png')
-                    icons[h] = dest
-        except OSError as e:
-            print(os.path.relpath(icon_path, steam_root), ":", e, file=sys.stderr)
-            # Actually, an .ico file can be a zip archive
-            if zipfile.is_zipfile(icon_path):
-                print(os.path.relpath(icon_path, steam_root), 'appears to be a zip file')
-                zipped_icons = extract_zipped_icons(tmpdir=tmpdir, icon_hash=icon_hash, icon_path=icon_path)
-                icons.update(zipped_icons)
-    else:
-        raise ValueError('Don\'t know how to handle', icon_path)
-    return icons
-
-
-def get_icons(steam_root, app_info):
-    """
-    Extracts icons from steam directory in either format
-    and return paths of extracted icons
+    Get name of the file containing icon(s)
     """
     common_info = app_info['sections'][b'appinfo'][b'common']
     icons_dir = os.path.join(steam_root, 'steam', 'games')
@@ -90,9 +36,51 @@ def get_icons(steam_root, app_info):
                 icon_path = os.path.join(icons_dir, f'{icon_hash}.{fmt}')
                 if os.path.isfile(icon_path):
                     print('found', os.path.relpath(icon_path, steam_root), file=sys.stderr)
-                    return extract_icons(steam_root, icon_hash, icon_path)
+                    return (icon_hash, icon_path)
             print('not found', file=sys.stderr)
-    return None
+    return (None, None)
+
+
+def extract_icon_source(icon_source, destdir, icon_name):
+    if zipfile.is_zipfile(icon_source):
+        print(os.path.basename(icon_source), 'appears to be a zip file', file=sys.stderr)
+        with zipfile.ZipFile(icon_source, 'r') as zf:
+            for zi in zf.infolist():
+                if not zi.is_dir() and zi.filename.endswith('.png'):
+                    with zf.open(zi.filename) as img_file:
+                        print('Saving icon', zi.filename, file=sys.stderr)
+                        save_icon(img_file, destdir, icon_name)
+    elif icon_source.endswith('.ico'):
+        print('Saving icon', icon_source, file=sys.stderr)
+        with open(icon_source, 'rb') as img_file:
+            save_icon(img_file, destdir, icon_name)
+
+
+def save_icon(img_file, destdir, icon_name):
+    """
+    Save given bytes-like object to given directory with given name
+    """
+    # FIXME we create here a new bytes-like objects because ZipExtFile is not seekable
+    with io.BytesIO() as img_bytes:
+        img_bytes.write(img_file.read())
+        try:
+            img = Image.open(img_bytes)
+        except OSError as e:
+            print(e, file=sys.stderr)
+        else:
+            h, w = img.size
+            if h == w:
+                sized_destdir = os.path.join(destdir, 'icons', 'hicolor', f'{h}x{w}', 'apps')
+                os.makedirs(sized_destdir, exist_ok=True)
+                dest = os.path.join(sized_destdir, f'{icon_name}.png')
+                if img.format == 'PNG':
+                    with open(dest, 'wb') as df:
+                        img_bytes.seek(0)
+                        df.write(img_bytes.read())
+                else:
+                    img.save(dest)
+                img.close()
+                return(dest)
 
 
 def create_desktop_data(steam_root, destdir=None, steam_cmd='xdg-open'):
@@ -114,14 +102,10 @@ def create_desktop_data(steam_root, destdir=None, steam_cmd='xdg-open'):
             app_name = app_info['sections'][b'appinfo'][b'common'][b'name'].decode()
             print('Processing app ID', app_id, ':', app_name, file=sys.stderr)
 
-            app_icons = get_icons(steam_root, app_info)
+            app_icon_hash, app_icon_src = get_icon_source(steam_root, app_info)
             app_icon_name = f'steam_icon_{app_id}'
-            if app_icons is not None:
-                for size, icon_src in app_icons.items():
-                    icon_dest_dir = os.path.join(destdir, 'icons', 'hicolor', f'{size}x{size}', 'apps')
-                    icon_dest = os.path.join(icon_dest_dir, f'{app_icon_name}.png')
-                    os.makedirs(icon_dest_dir, exist_ok=True)
-                    shutil.copyfile(icon_src, icon_dest)
+            if app_icon_src is not None:
+                extract_icon_source(icon_source=app_icon_src, destdir=destdir, icon_name=app_icon_name)
 
             app_desktop_file = f'steam_app_{app_id}.desktop'
             app_desktop = ConfigParser()
@@ -134,7 +118,9 @@ def create_desktop_data(steam_root, destdir=None, steam_cmd='xdg-open'):
                 'Icon': app_icon_name,
                 'Categories': 'Game;X-Steam;'
             }
-            with open(os.path.join(destdir, 'applications', app_desktop_file), 'w') as df:
+            apps_destdir = os.path.join(destdir, 'applications')
+            os.makedirs(apps_destdir, exist_ok=True)
+            with open(os.path.join(apps_destdir, app_desktop_file), 'w') as df:
                 app_desktop.write(df, space_around_delimiters=False)
 
 
