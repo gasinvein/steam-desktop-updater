07f0acc84ad2aa9eed77dc5745b7149a9079c740
gasinvein <gasinvein@gmail.com>
2020-09-15 18:35:50 +0300

    Make save_icon a method

--- steam_desktop_updater.py
+++ steam_desktop_updater.py
@@ -56,7 +56,7 @@ class SteamApp(object):
         with open(os.path.join(apps_destdir, app_desktop_file), 'w') as df:
             app_desktop.write(df, space_around_delimiters=False)
 
-    def get_icon_store(self):
+    def get_icon_extractor(self, datadir, icon_name):
         """
         Get name of the file containing icon(s)
         """
@@ -70,19 +70,23 @@ class SteamApp(object):
                     icon_path = os.path.join(icons_dir, f'{icon_hash}.{fmt}')
                     if os.path.isfile(icon_path):
                         logging.debug(f'found {icon_path}')
-                        return SteamIconStore(icon_path)
+                        return SteamIconExtractor(icon_path, datadir, icon_name)
 
     def extract_icons(self, destdir):
-        icon_store = self.get_icon_store()
+        icon_store = self.get_icon_extractor(datadir=destdir, icon_name=self.icon_name)
         if icon_store is not None:
-            icon_store.extract_icons(destdir=destdir, icon_name=self.icon_name)
+            icon_store.extract_icons()
+        else:
+            logging.warning(f'No icons found')
 
 
-class SteamIconStore(object):
-    def __init__(self, icon_file):
+class SteamIconExtractor(object):
+    def __init__(self, icon_file, datadir, icon_name):
         self._file = icon_file
+        self.datadir = datadir
+        self.icon_name = icon_name
 
-    def extract_icons(self, destdir, icon_name):
+    def extract_icons(self):
         if zipfile.is_zipfile(self._file):
             logging.info(f'{self._file} appears to be a zip file')
             with zipfile.ZipFile(self._file, 'r') as zf:
@@ -90,50 +94,52 @@ class SteamIconStore(object):
                     if not zi.is_dir() and zi.filename.endswith('.png'):
                         logging.info(f'Saving icon {zi.filename}')
                         with zf.open(zi.filename) as img_file:
-                            save_icon(img_file, destdir, icon_name)
+                            self.save_icon(img_file)
         elif self._file.endswith('.ico'):
             logging.info(f'Saving icon {self._file}')
             with open(self._file, 'rb') as img_file:
-                save_icon(img_file, destdir, icon_name)
+                self.save_icon(img_file)
 
-
-def save_icon(img_file, destdir, icon_name):
-    """
-    Save given bytes-like object to given directory with given name
-    """
-    try:
-        img = Image.open(img_file)
-    except OSError as e:
-        logging.info(e)
-    else:
+    def save_icon(self, img_file):
+        """
+        Save given bytes-like object to given directory with given name
+        """
+        try:
+            img = Image.open(img_file)
+        except OSError as e:
+            logging.info(e)
+            return
         h, w = img.size
         save_direct = True
-        if h == w:
-            s = h
-            # Resize icon if it's too large
-            m = ICON_SIZES['max']
-            max_size_dest = os.path.join(destdir, 'icons', 'hicolor', f'{m}x{m}', 'apps', f'{icon_name}.png')
-            if s > m:
-                if os.path.isfile(max_size_dest):
-                    return
-                logging.info(f'Icon size {s}x{s} is too large, resizing')
-                new_img = img.resize((m, m), resample=Image.LANCZOS)
-                img.close()
-                img = new_img
-                s = m
-                save_direct = False
-            if img.format != 'PNG':
-                save_direct = False
-            dest = os.path.join(destdir, 'icons', 'hicolor', f'{s}x{s}', 'apps', f'{icon_name}.png')
-            os.makedirs(os.path.dirname(dest), exist_ok=True)
-            if save_direct:
-                with open(dest, 'wb') as df:
-                    img_file.seek(0)
-                    df.write(img_file.read())
-            else:
-                img.save(dest)
+        if h != w:
+            return
+        s = h
+        # Resize icon if it's too large
+        m = ICON_SIZES['max']
+        max_size_dest = os.path.join(self.datadir, 'icons', 'hicolor', f'{m}x{m}', 'apps',
+                                     f'{self.icon_name}.png')
+        if s > m:
+            if os.path.isfile(max_size_dest):
+                return
+            logging.info(f'Icon size {s}x{s} is too large, resizing')
+            new_img = img.resize((m, m), resample=Image.LANCZOS)
             img.close()
-            return(dest)
+            img = new_img
+            s = m
+            save_direct = False
+        if img.format != 'PNG':
+            save_direct = False
+        dest = os.path.join(self.datadir, 'icons', 'hicolor', f'{s}x{s}', 'apps', f'{self.icon_name}.png')
+        os.makedirs(os.path.dirname(dest), exist_ok=True)
+        if save_direct:
+            with open(dest, 'wb') as df:
+                img_file.seek(0)
+                df.write(img_file.read())
+        else:
+            img.save(dest)
+        img.close()
+        return(dest)
+
 
 def get_installed_apps(library_folder):
     """
