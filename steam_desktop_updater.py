13999151ad8c8bab98c81570a5d0cd14ed7e7b7b
Peter Oliver <git@mavit.org.uk>
2020-11-21 14:59:27 +0000

    Avoid potential collision with an item with key 0 in libraryfolders.vdf

--- steam_desktop_updater.py
+++ steam_desktop_updater.py
@@ -167,10 +167,7 @@ def get_installed_apps(steam_root):
     apps = []
     logging.info('Searching library folders')
     with open(os.path.join(steam_root, 'steamapps', 'libraryfolders.vdf'), 'r') as lf:
-        library_folders = vdf.load(lf)['LibraryFolders']
-        assert '0' not in library_folders
-        library_folders['0'] = steam_root
-        for k, v in library_folders.items():
+        for k, v in [("0", steam_root)] + list(vdf.load(lf)['LibraryFolders'].items()):
             if not k.isdigit():
                 continue
             logging.info(f'Collecting apps in folder {v}')
